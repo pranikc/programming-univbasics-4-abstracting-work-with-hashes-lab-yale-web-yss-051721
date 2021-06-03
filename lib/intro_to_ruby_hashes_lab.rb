e88a87155e0d2f0f06ea07caee9ef22a7c1a698f
abstract-instructions-1268 <pranik.chainani@yale.edu>
2021-06-03 20:27:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,4 @@
-def my_hash_creator(key, value)
+def my_hash_creator(name, value)
   # return a hash that includes the key and value parameters passed into this method
   { key: value }
 end
@@ -7,7 +7,6 @@ def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   hash[key]
 end
-
 
 def update_counting_hash(hash, key)
   if hash[key]
